d729ebb2ebf05f3a73614f3fb13c174c698149de
tstan <tstan@LAPTOP-DIIV40FR>
2022-01-19 13:55:11 -0500

    Add project files.

---
+++ CIS170C-LAB2B/Main.cpp
@@ -0,0 +1,26 @@
+// ---------------------------------------------------------------
+// Programming Assignment:	LAB2B
+// Developer:			          Travis J. Stanfield
+// Date Written:			11/5/2020
+// Purpose:				Calorie Counter 
+// ---------------------------------------------------------------
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	
+	cout << "\nCalorie Counter\n"<<endl;
+	for (int i = 10,mins = 5; i <= 30;  i = i + 5)
+		{
+		mins <= 30;
+		mins = mins + 5;
+				
+		cout << i * 3.9 << " calories burned after " << mins << " minutes.\n" << endl;
+				
+		}
+
+}
